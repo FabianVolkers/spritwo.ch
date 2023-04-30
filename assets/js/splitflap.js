711bed34e7139a19396914df34a370f51a8f1c10
Fabian Volkers <33686912+FabianVolkers@users.noreply.github.com>
2023-04-30 12:06:34 +0200

    Extract method

--- assets/js/splitflap.js
+++ assets/js/splitflap.js
@@ -106,25 +106,10 @@ document.addEventListener("DOMContentLoaded", function () {
     let now = new Date();
     const remainingTime = targetDate - now;
 
-    const lettersNumbersSpecialChars = [
-      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-      ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', ',', '<', '.', '>', '/', '?', '|', '\\', '`', '~'
-    ];
+    if(remainingTime > 0) {
+      const timeString = ` ${getTimeString(remainingTime)} `;
 
-    const timeString = ` ${getTimeString(remainingTime)} `;
-
-    for (let i = 0; i < timeString.length; ++i) {
-      let letter = timeString[i];
-      let j = lettersNumbersSpecialChars.indexOf(splitFlaps[i].letter());
-      while (letter !== lettersNumbersSpecialChars[j]) {
-        ++j;
-        if (j >= lettersNumbersSpecialChars.length) {
-          j = 0;
-        }
-        splitFlaps[i].to(lettersNumbersSpecialChars[j], 10, 0);
-      }
-
+      animateSplitFlapsToString(timeString, splitFlaps);
     }
 
     beginCountdown();
@@ -135,25 +120,49 @@ document.addEventListener("DOMContentLoaded", function () {
 
     setInterval(() => {
       let now = new Date();
-      splitFlaps[0].to(" ");
-      splitFlaps[9].to(" ");
-      const remainingTime = targetDate - now;
 
+      const remainingTime = targetDate - now;
+      console.log(remainingTime)
       if (remainingTime < 0) {
-        let dateStr = "ABFAHRT!";
-        for (let i = 0; i < dateStr.length; ++i) {
-          splitFlaps[i + 1].to(dateStr[i]);
-        }
+        let dateStr = " ABFAHRT! ";
+        animateSplitFlapsToString(dateStr, splitFlaps);
+        // for (let i = 0; i < dateStr.length; ++i) {
+        //   splitFlaps[i + 1].to(dateStr[i]);
+        // }
         return;
       }
+      // splitFlaps[0].to(" ");
+      // splitFlaps[9].to(" ");
 
-      let dateStr = getTimeString(remainingTime);
+      let dateStr = ` ${getTimeString(remainingTime)} `;
       for (let i = 0; i < dateStr.length; ++i) {
-        splitFlaps[i + 1].to(dateStr[i]);
+        splitFlaps[i].to(dateStr[i]);
       }
     }, 1000);
   }
 });
+function animateSplitFlapsToString(timeString, splitFlaps) {
+  console.log("Animate to: " + timeString);
+  const lettersNumbersSpecialChars = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', ',', '<', '.', '>', '/', '?', '|', '\\', '`', '~'
+  ];
+
+  for (let i = 0; i < timeString.length; ++i) {
+    let letter = timeString[i];
+    let j = lettersNumbersSpecialChars.indexOf(splitFlaps[i].letter());
+    while (letter !== lettersNumbersSpecialChars[j]) {
+      ++j;
+      if (j >= lettersNumbersSpecialChars.length) {
+        j = 0;
+      }
+      splitFlaps[i].to(lettersNumbersSpecialChars[j], 10, 0);
+    }
+
+  }
+}
+
 function getTimeString(milliseconds) {
   const hours = Math.floor(milliseconds / (1000 * 60 * 60));
   const minutes = Math.floor(

90f539baaa264dcbb5c047ba1327f70633f8ed1d
Fabian Volkers <33686912+FabianVolkers@users.noreply.github.com>
2023-05-02 19:16:51 +0200

    Don't delete old events

--- fetch_calendar_events.py
+++ fetch_calendar_events.py
@@ -4,6 +4,21 @@ from datetime import datetime
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
+def load_existing_events(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+def save_updated_events(file_path, existing_events, new_events):
+    for new_event in new_events:
+        event_id = new_event['id']
+        existing_events[event_id] = new_event
+
+    with open(file_path, 'w') as f:
+        json.dump(existing_events, f, indent=2)
+
 # Set up Google Calendar API
 calendar_id = os.environ["GOOGLE_CALENDAR_ID"]
 service_account_key_file = "service_account_key.json"
@@ -11,6 +26,9 @@ service_account_key_file = "service_account_key.json"
 creds = service_account.Credentials.from_service_account_file(service_account_key_file, scopes=['https://www.googleapis.com/auth/calendar.readonly'])
 
 calendar_service = build('calendar', 'v3', credentials=creds)
+
+# Load existing events from the JSON file
+existing_events = load_existing_events('calendar_events.json')
 
 # Get events from the Google Calendar
 now = datetime.utcnow().isoformat() + 'Z'
@@ -20,7 +38,6 @@ events_results = calendar_service.events().list(calendarId=calendar_id,
                                                 singleEvents=True,
                                                 orderBy='startTime').execute()
 
-# Write events to a JSON file
-events = events_results.get('items', [])
-with open('calendar_events.json', 'w') as f:
-    json.dump(events, f, indent=2)
+# Update existing events with new or changed events
+new_events = events_results.get('items', [])
+save_updated_events('calendar_events.json', existing_events, new_events)

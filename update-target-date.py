9ef96c05d192cfd5cf0f4179c2524e90a1eef376
Fabian Volkers <33686912+FabianVolkers@users.noreply.github.com>
2023-05-02 19:28:52 +0200

    Skip if there are no upcoming events

--- update-target-date.py
+++ update-target-date.py
@@ -8,7 +8,9 @@ def load_upcoming_event_date(file_path):
         now = datetime.utcnow()
         event_dates = [datetime.fromisoformat(event['start'].get('dateTime') or event['start'].get('date')) for event in events.values()]
         future_event_dates = [event_date for event_date in event_dates if event_date > now]
-        upcoming_event_date = min(future_event_dates)
+        upcoming_event_date = None
+        if len(future_event_dates) > 0:
+            upcoming_event_date = min(future_event_dates)
         return upcoming_event_date
 
 def update_target_date(file_path, target_date):
@@ -25,4 +27,5 @@ upcoming_event_date = load_upcoming_event_date('calendar_events.json')
 upcoming_event_date = upcoming_event_date.replace(microsecond=0).isoformat()
 
 # Update the target_date in index.markdown
-update_target_date('index.markdown', upcoming_event_date)
+if upcoming_event_date:
+    update_target_date('index.markdown', upcoming_event_date)

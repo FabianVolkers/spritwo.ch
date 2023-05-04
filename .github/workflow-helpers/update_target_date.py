0fa6883a5dca116cd7e84ddb8ae2dc463f465cae
Fabian Volkers <33686912+FabianVolkers@users.noreply.github.com>
2023-05-04 19:34:26 +0200

    Write event_location to markdown files

--- .github/workflow-helpers/update_target_date.py
+++ .github/workflow-helpers/update_target_date.py
@@ -3,45 +3,57 @@ import re
 import pytz
 from datetime import datetime
 
-def load_upcoming_event_date(file_path):
+def load_upcoming_event(file_path):
     with open(file_path, 'r') as f:
-        events = json.load(f)
+        events_raw = json.load(f)
         now = datetime.utcnow()
         now = now.astimezone(pytz.utc)
 
-        event_dates = []
-        for event in events.values():
+        events = []
+        for event in events_raw.values():
             event_start = event['start'].get('dateTime') or event['start'].get('date')
             event_timezone = event['start'].get('timeZone')
             event_start = datetime.fromisoformat(event_start)
             if event_timezone:
                 timezone = pytz.timezone(event_timezone)
                 event_start = event_start.astimezone(timezone)
-            event_dates.append(event_start)
 
-        future_event_dates = [event_date for event_date in event_dates if event_date > now]
-        upcoming_event_date = None
+            # Set the event location to the event location if it exists, otherwise set it to 'Campus'
+            event_location = event.get('location') or 'Campus'
 
-        if len(future_event_dates) > 0:
-            upcoming_event_date = min(future_event_dates)
-        elif len(event_dates) > 0:
-            upcoming_event_date = max(event_dates)
+            events.append({
+                "event_start": event_start,
+                "event_location": event_location
+                })
 
-        return upcoming_event_date
+        future_events = [event for event in events if event["event_start"] > now]
+        upcoming_event = None
 
-def update_target_date(file_path, target_date):
+        if len(future_events) > 0:
+            upcoming_event = min(future_events, key=lambda event: event['event_start'])
+        elif len(events) > 0:
+            upcoming_event = max(events, key=lambda event: event['event_start'])
+
+        return upcoming_event
+
+def update_target_date_and_location(file_path, target_date, event_location):
     with open(file_path, 'r') as f:
         content = f.read()
 
+    # Update the target_date in index.markdown
     updated_content = re.sub(r"target_date: (\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2})", f"target_date: {target_date}", content)
-
+    # Update event_location line in index.markdown
+    updated_content = re.sub(r"event_location: (.*)", f"event_location: {event_location}", updated_content)
     with open(file_path, 'w') as f:
         f.write(updated_content)
 
-# Load the upcoming event date from calendar_events.json
-upcoming_event_date = load_upcoming_event_date('.github/workflow-helpers/calendar_events.json')
-upcoming_event_date = upcoming_event_date.replace(microsecond=0).isoformat()
+# Load the upcoming event from calendar_events.json
+upcoming_event = load_upcoming_event('.github/workflow-helpers/calendar_events.json')
+upcoming_event["event_start"] = upcoming_event["event_start"].replace(microsecond=0).isoformat()
+
 
 # Update the target_date in index.markdown
-if upcoming_event_date:
-    update_target_date('index.markdown', upcoming_event_date)
+if upcoming_event:
+    for filename in ['index.markdown', 'preview.markdown']:
+        update_target_date_and_location(filename, upcoming_event["event_start"], upcoming_event["event_location"])
+    # update_target_date_and_location('index.markdown', upcoming_event["event_start"], upcoming_event["event_location"])
